b0d63527f5e77568b9c25c4df6ab77c375677a41
chaesanamidaarumdapta <113958419+chaesanamidaarumdapta@users.noreply.github.com>
2022-09-25 10:25:59 +0700

    Add files via upload

---
+++ latihan 1 prak 2.py
@@ -0,0 +1,10 @@
+import math
+a = float(input("masukkan angka A="))
+b = float(input("masukkan angka B="))
+print("hasil penjumlahan=", a+b)
+print("hasil pengurangan=", a-b)
+print("hasil perkalian=", a*b)
+print("hasil sisa pembagian=", a%b)
+print("hasil pembagian=", a/b)
+print("hasil pemangkatan=", a**b)
+print("hasil logaritma=", math.log10(a))
